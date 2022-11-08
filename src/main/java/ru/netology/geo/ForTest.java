5969073899a4676c084978818475718d572706c9
Andrei Grazhdan <myway@yandex-team.ru>
2022-11-08 23:18:43 +0700

    написал тест

--- src/main/java/ru/netology/geo/ForTest.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.geo;
-
-public class ForTest {
-    private int testData = 1;
-
-    public int getTestData(){
-        return testData;
-    }
-}
--- src/test/java/TestMessSender.java
+++ src/test/java/TestMessSender.java
@@ -3,49 +3,97 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
-import ru.netology.geo.ForTest;
+
 import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
 import ru.netology.i18n.LocalizationServiceImpl;
 import ru.netology.sender.MessageSender;
 import ru.netology.sender.MessageSenderImpl;
+
 import static org.mockito.Mockito.*;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.entity.Country.RUSSIA;
-import static ru.netology.geo.GeoServiceImpl.MOSCOW_IP;
+import static ru.netology.entity.Country.USA;
+import static ru.netology.geo.GeoServiceImpl.*;
 import static ru.netology.sender.MessageSenderImpl.IP_ADDRESS_HEADER;
 
 public class TestMessSender {
 
     @Test
-    public void langRus(){
-       // GeoServiceImpl geoService = new GeoServiceImpl();
+    public void test_langRus() {
         GeoService geoService = Mockito.mock(GeoService.class);
-        Country russia = RUSSIA;
-        String ip = MOSCOW_IP;
-        Mockito.when(geoService.byIp(ip))
-                .thenReturn(new Location("M", russia, "lenina", 3));
-
-      //  LocalizationServiceImpl localizationService = new LocalizationServiceImpl();
-        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
         Country country = RUSSIA;
-        String rusRezult = "Добро пожаловать";
-        Mockito.when(localizationService.locale(country)).thenReturn(rusRezult);
+        String ipRuSegment = "172.X.XX.XX";
+        //  String ipAmerSegment = "96.X.XX.XX";
+        Mockito.when(geoService.byIp(ipRuSegment))
+                .thenReturn(new Location("test", country, "test", 0));
+
+        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
+
+        String expected = "Добро пожаловать";
+        Mockito.when(localizationService.locale(country)).thenReturn(expected);
 
         MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
 
         Map<String, String> headers = new HashMap<String, String>();
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, MOSCOW_IP);
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ipRuSegment);
         String real = messageSender.send(headers);
-        Assertions.assertEquals(rusRezult, real);
+        Assertions.assertEquals(expected, real);
     }
-     @Test
+
+    @Test
+    public void test_langEng() {
+        GeoService geoService = Mockito.mock(GeoService.class);
+        Country country = USA;
+        String ipAmerSegment = "96.X.XX.XX";
+        Mockito.when(geoService.byIp(ipAmerSegment))
+                .thenReturn(new Location("test", country, "test", 0));
+
+        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
+        String expected = "Welcome";
+        Mockito.when(localizationService.locale(country)).thenReturn(expected);
+
+        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, ipAmerSegment);
+        String real = messageSender.send(headers);
+        Assertions.assertEquals(expected, real);
+    }
+
+    @Test
+    public void test_location() {
+        // метод Location byIp(String ip) - имеет пять условий. Необходимо проверить каждое из них.
+        // результат работы метода - локация. Так как все экземпляры локации имеют одинаковое количество полей
+        // я проверю работоспособность метода по любому выбранному полю, например четвертому.
+        String[] hosts = {LOCALHOST, MOSCOW_IP, NEW_YORK_IP, "172.X.XX.XX", "96.X.XX.XX"};
+        int[] buildings = {0, 15, 32, 0, 0};
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        for (int i = 0; i < 5; i++) {
+            Location actualLoc = geoService.byIp(hosts[i]);
+            Object actual = actualLoc.getBuiling();
+            int expected = buildings[i];
+          //  System.out.println(expected);
+            Assertions.assertEquals(expected, actual);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+/*     @Test
     public void testMockito(){
          ForTest forTest = Mockito.mock(ForTest.class);
          Mockito.when(forTest.getTestData()).thenReturn(2);
@@ -60,6 +108,6 @@ public class TestMessSender {
         int value = forTest.getTestData();
         int expected = 1;
         Assertions.assertEquals(expected, value);
-    }
+    }*/
 
 }
